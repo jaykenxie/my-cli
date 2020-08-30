1384c419876b85e727abf962494443c613a47196
jayken.xie <jaykenxie@outlook.com>
2020-08-30 15:54:00 +0800

    补全注释

--- bin/shanx.js
+++ bin/shanx.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
-const cmd = require('commander');
+const program = require('commander');
 const { version } = require('../package.json');
 const init = require('../lib/init');
-cmd.version(version);
 
-cmd
-  .command('init <name>')
-  .description('init project')
-  .action((name) => init(name));
+// 设置版本信息
+program.version(version);
 
-cmd.parse(process.argv);
+program
+  .command('init <name>') // 定义命令
+  .description('init project') // 命令描述信息
+  .action((name) => init(name)); // 处理指令 回调函数中的name 就是命令第三个参数的值 如：shanx init demo 那么name就等于demo
+
+program.parse(process.argv); // 通过program.parse(arguments)方法处理参数，没有被使用的选项会存放在program.args数组中。
